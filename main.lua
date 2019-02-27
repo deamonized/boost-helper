b4dbfbf9bd6ef50b1cfac880b3414a43d80a0b32
Harley Baker <deamonized123@gmail.com>
2019-02-27 07:55:29 +0000

    add first working version

---
+++ main.lua
@@ -0,0 +1,53 @@
+BoostHelper = LibStub("AceAddon-3.0"):NewAddon("BoostHelper", "AceConsole-3.0")
+GUI = LibStub("AceGUI-3.0", true)
+BoostHelper:RegisterChatCommand("bh",  "CommandProcessor")
+BoostHelper:RegisterChatCommand("boosthelper",  "CommandProcessor")
+
+function BoostHelper:OnInitialize()
+
+end
+
+function BoostHelper:OnEnable()
+
+end
+
+function BoostHelper:OnDisable()
+
+end
+
+function BoostHelper:CommandProcessor(input)
+    if input == "names" then
+        BoostHelper:ListProcessor()
+    else
+        print("Usage:")
+        print("/boosthelper names - Gives a list of all players in the raid")
+    end
+end
+
+
+function BoostHelper:ListProcessor()
+    local raidNamesMessage = ""
+    local raidNames = {}
+    for i=1,40 do
+        local raiderName = GetRaidRosterInfo(i)
+        if raiderName == nil then
+            break
+        end
+        if raiderName:match("-")== nil then
+            raiderName = raiderName .. "-"
+            raiderName = raiderName .. GetRealmName()
+        end
+        raidNames[#raidNames + 1] = raiderName
+    end
+
+    raidNamesMessage = table.concat(raidNames, "\n")
+
+    RaidListCopyFrame:Show()
+    RaidListCopyFrameScroll:Show()
+    RaidListCopyFrameScrollText:Show()
+    RaidListCopyFrameScrollText:SetText(raidNamesMessage)
+    RaidListCopyFrameScrollText:HighlightText()
+    RaidListCopyFrameScrollText:SetScript("OnEscapePressed", function(self)
+        RaidListCopyFrame:Hide()
+    end)
+end
